46bd14b7941f1c430d1f1f4c89439af4a89fbf30
slay-s <slay_s@outlook.jp>
2014-05-25 23:46:24 +0900

    テスト刀工

---
+++ src/testApp/Adrd_testApp.java
@@ -0,0 +1,22 @@
+package testApp;
+ 
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+
+public class Adrd_testApp {
+	public static void main(String args[])throws TwitterException {
+		//twitter4jのTwitterクラス
+		Twitter twitter = new TwitterFactory().getInstance();
+		User user = twitter.verifyCredentials();
+		//ユーザ情報取得
+		System.out.println("名前　　　　：" + user.getName());
+		System.out.println("表示名　　　：" + user.getScreenName());
+		System.err.println("フォロー数　：" + user.getFriendsCount());
+		System.out.println("フォロワー数：" + user.getFollowersCount());
+		//つぶやきの実行
+		Status status = twitter.updateStatus("HeLL java Twitter!!");
+	}	
+}
